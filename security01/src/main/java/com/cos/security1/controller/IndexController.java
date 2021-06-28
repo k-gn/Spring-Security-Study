9ab4262e2efb41daafb9fa0d7e85d3df64e2f182
k-gn <rlarbska97@gmail.com>
2021-06-28 22:38:24 +0900

    시큐리티 기초

---
+++ security01/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,46 @@
+package com.cos.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/mamnager")
+    public String mamnager() {
+        return "mamnager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc() {
+        return "회원가입 완료!";
+    }
+
+}
