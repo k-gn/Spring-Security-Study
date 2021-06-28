fc9e77444117c2b230401d5970ff019ee94cbd0b
k-gn <rlarbska97@gmail.com>
2021-06-29 00:01:09 +0900

    시큐리티 기초

--- security01/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security01/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록된다.
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // 시큐리티 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -29,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin()
                 .loginPage("/login")
-                .loginProcessingUrl("/loginProc");
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/");
 
 
     }
--- security01/src/main/java/com/cos/security1/controller/IndexController.java
+++ security01/src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,16 +25,19 @@ public class IndexController {
     }
 
     @GetMapping("/user")
+    @ResponseBody
     public String user() {
         return "user";
     }
 
     @GetMapping("/admin")
+    @ResponseBody
     public String admin() {
         return "admin";
     }
 
     @GetMapping("/mamnager")
+    @ResponseBody
     public String mamnager() {
         return "mamnager";
     }
@@ -55,4 +61,18 @@ public class IndexController {
         return "redirect:/login";
     }
 
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    @ResponseBody
+    public String info() {
+        return "info";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+//    @PostAuthorize("")
+    @GetMapping("/data")
+    @ResponseBody
+    public String data() {
+        return "data";
+    }
 }
