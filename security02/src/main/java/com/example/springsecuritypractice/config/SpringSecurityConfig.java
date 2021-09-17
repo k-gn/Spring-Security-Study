e07754e4c1af63588eef764a8a24fc38621c334f
k-gn <rlarbska97@gmail.com>
2021-09-17 11:29:46 +0900

    no message

--- security02/src/main/java/com/example/springsecuritypractice/config/SpringSecurityConfig.java
+++ security02/src/main/java/com/example/springsecuritypractice/config/SpringSecurityConfig.java
@@ -2,7 +2,11 @@ package com.example.springsecuritypractice.config;
 
 import com.example.springsecuritypractice.filter.StopwatchFilter;
 import com.example.springsecuritypractice.filter.TestAuthenticationFilter;
+import com.example.springsecuritypractice.jwt.JwtAuthenticationFilter;
+import com.example.springsecuritypractice.jwt.JwtAuthorizationFilter;
+import com.example.springsecuritypractice.jwt.JwtProperties;
 import com.example.springsecuritypractice.user.User;
+import com.example.springsecuritypractice.user.UserRepository;
 import com.example.springsecuritypractice.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -14,9 +18,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -31,24 +37,37 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     // 스프링 시큐리티 규칙 대부분을 설정하는 메소드 (웹 기반 보안을 구성)
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // stateless (토큰 방식은 세션이 필요없음)
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 정책을 사용하지 않음
+        // jwt filter
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(this.authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class
+        ).addFilterBefore( // 체이닝 가능
+                new JwtAuthorizationFilter(userRepository),
+                BasicAuthenticationFilter.class
+        );
+
         // add filter
-        http.addFilterBefore(new StopwatchFilter(), WebAsyncManagerIntegrationFilter.class);
-        http.addFilterBefore(new TestAuthenticationFilter(this.authenticationManager()), UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(new StopwatchFilter(), WebAsyncManagerIntegrationFilter.class);
+//        http.addFilterBefore(new TestAuthenticationFilter(this.authenticationManager()), UsernamePasswordAuthenticationFilter.class);
         // basic authentication : 요청마다 username, password 데이터를 포함시켜서 보내는 방식 (세션이 필요없지만 보안에 취약해서 https 사용 권장)
         http.httpBasic().disable(); // basic authentication filter 비활성화
         // csrf
-        http.csrf();
+//        http.csrf();
         // anonymous 설정, 인증이 안된 유저가 요청을 하거나 들어올 경우 시큐리티는 익명 유저로 만들어 authentication 객체 넣어준다.
 //        http.anonymous().principal("");
         // remember-me
-        http.rememberMe(); // 자동 로그인 (remember-me 쿠키 발행), rememberMeAuthenticationFilter 활성화
+//        http.rememberMe(); // 자동 로그인 (remember-me 쿠키 발행), rememberMeAuthenticationFilter 활성화
         // 경로별 권한 설정    
         // authorization, 인가 설정 시작
-        http.authorizeRequests() 
+        http.authorizeRequests()
                 // /와 /home, /signup 은 모두에게 허용
                 .antMatchers("/", "/home", "/signup").permitAll()
                 .antMatchers("/note").hasRole("USER")
@@ -65,7 +84,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         // requestMatchers : 명확하게 요청 대상을 지정하는 경우 사용
         http.logout() // 로그아웃 설정
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 요청 경로
-                .logoutSuccessUrl("/"); // 로그아웃 성공 시 이동할 url
+                .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 url
+                .invalidateHttpSession(true) // 로그 아웃시 인증정보를 지우하고 세션을 무효화
+                .deleteCookies(JwtProperties.COOKIE_NAME); // 로그 아웃시 해당 쿠키 제거
     }
 
     // 전역 보안에 영향을주는 구성 설정에 사용됩니다 (자원 무시, 디버그 모드 설정, 사용자 지정 방화벽 정의를 구현하여 요청 거부 등)
